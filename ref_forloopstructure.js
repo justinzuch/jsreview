937e2043823ac984d053d605ac5f03e6abb92ef4
Justin Z <justin.zuchowski@gmail.com>
2016-05-08 12:28:05 -0600

    Create ref_forloopstructure.js
    Just for quick reference on "for" loop structure. Nothing too extreme here.

---
+++ ref_forloopstructure.js
@@ -0,0 +1,19 @@
+// Structure for "for" loops for reference.
+// "i" will be our starting point/counter, it can be called something else.
+// The loop will run up until 100.
+// It will 
+
+for (i = 0; i <= 100; i+=5) {
+  console.log(i);
+// Just a consolelog return right now to test in console.
+}
+
+
+
+// Decreasing from 100 to 0 by 5 will look like this...
+
+for (i = 100; i >=0; i-=5) {
+  console.log(i);
+  }
+  
+  // Tested in console and both work.
