b10c5737c7dc838208a6649831e6baebe7da00f4
Justin Z <justin.zuchowski@gmail.com>
2016-06-21 18:37:42 -0600

    Create wdbExercise.js

---
+++ wdbExercise.js
@@ -0,0 +1,27 @@
+//Exercise using if, else if, and else statements.
+
+//If age is - then print a error message.
+//If age is 21 then print "Happy 21st Birthday!".
+//If age is off then print "Your age is odd!".
+//Bonus: If age is a perfect square print "perfect square!"
+
+if(age > 0) {
+  console.log("Age cannot be negative!");
+  }
+else if(age === 21) {
+  console.log("Happy 21st Birthday!");
+  }
+else if(!(age/2)) {
+  console.log("Your age is odd!");
+  }
+else if(Math.sqrt(age) {
+  console.log("Perfect Square!");
+  }
+  
+  
+  
+  /*
+  // If age is negative  if(age < 0){      console.log("Come back once you're out of the womb");  }      // If age is 21  if(age === 21){      console.log("Happy 21st Birthday!");  }      
+  // If age is odd  //(not evenly divisible by two)  if(age % 2 !== 0) {      console.log("Your age is odd!");  }
+  
+  */
