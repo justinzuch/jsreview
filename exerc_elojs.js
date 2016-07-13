5860583edd44aaa636d8ab8df99bb7dbb6923230
Justin Z <justin.zuchowski@gmail.com>
2016-07-13 09:45:17 -0600

    Update exerc_elojs.js

--- exerc_elojs.js
+++ exerc_elojs.js
@@ -15,3 +15,11 @@ Output in console log with a loop.
 //Remembering it's a string, not a number value --> "#" gets added until length maxes at 7.
 for (var counter = "#"; counter.length <=7; counter = counter + "#")
 console.log(counter);
+
+
+/*Fizz Buzz Exercise from Chapter 2. 
+console.log all numbers from 1-100.
+Numbers divisible by 3 print "Fizz"
+Numbers divisible by 5 (and not 3) print "Buzz"
+*/
+
