e55f420693d165be5715e3bf6668b7c754059f2e
Justin Z <justin.zuchowski@gmail.com>
2016-07-13 09:35:46 -0600

    Update exerc_elojs.js
    Updated with console.log solution. Tested in console and it works.

--- exerc_elojs.js
+++ exerc_elojs.js
@@ -11,3 +11,7 @@ Output in console log with a loop.
 #######
 
 */
+//Soulution. .length to specify when the for loop should end.
+//Remembering it's a string, not a number value --> "#" gets added until length maxes at 7.
+for (var counter = "#"; counter.length <=7; counter = counter + "#")
+console.log(counter);
